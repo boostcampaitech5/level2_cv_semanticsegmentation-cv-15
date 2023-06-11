bd0e6391786e56a8ee1b9817b6e58909d3b7e6ef
SeoYoungDeok <dud1933@naver.com>
2023-06-11 14:41:25 +0900

    Feat : hausdorff loss 추가
    - hausdorff loss 추가

--- src/config/loss.py
+++ src/config/loss.py
@@ -2,13 +2,17 @@ from hydra.core.config_store import ConfigStore
 from torch.nn import BCEWithLogitsLoss
 
 from src.config import full_builds
-from src.loss import DiceBCELoss, DiceLoss
+from src.loss import DiceBCEHausdorffLoss, DiceBCELoss, DiceLoss
 
 BCEWithLogitsLossConfig = full_builds(BCEWithLogitsLoss)
 
 DiceLossConfig = full_builds(DiceLoss, smooth=1.0)
 
 DiceBCELossConfig = full_builds(DiceBCELoss, dice_smooth=1.0, bce_weight=0.5)
+
+DiceBCEHausdorffLossConfig = full_builds(
+    DiceBCEHausdorffLoss, dice_smooth=1.0, bce_weight=0.5
+)
 
 
 def _register_configs():
@@ -17,3 +21,4 @@ def _register_configs():
     cs.store(group="loss", name="bce_with_logits", node=BCEWithLogitsLossConfig)
     cs.store(group="loss", name="dice", node=DiceLossConfig)
     cs.store(group="loss", name="dice_bce", node=DiceBCELossConfig)
+    cs.store(group="loss", name="dice_bce_hausdorff", node=DiceBCELossConfig)
--- src/loss.py
+++ src/loss.py
@@ -1,4 +1,6 @@
+import torch
 import torch.nn.functional as F
+from torch import nn
 
 
 class DiceLoss:
@@ -26,4 +28,47 @@ class DiceBCELoss:
         pred = F.sigmoid(pred)
         dice = self.dice_loss(pred, target)
         loss = bce * self.bce_weight + dice * (1 - self.bce_weight)
+        return loss
+
+
+class AveragedHausdorffLoss(nn.Module):
+    def __init__(self):
+        super(nn.Module, self).__init__()
+
+    def forward(self, set1, set2):
+        assert set1.ndimension() == 2, "got %s" % set1.ndimension()
+        assert set2.ndimension() == 2, "got %s" % set2.ndimension()
+        assert set1.size()[1] == set2.size()[1], "got %s and %s." % (
+            set2.size()[1],
+            set2.size()[1],
+        )
+
+        d2_matrix = self.cdist(set1, set2)
+
+        # Modified Chamfer Loss
+        term_1 = torch.mean(torch.min(d2_matrix, 1)[0])
+        term_2 = torch.mean(torch.min(d2_matrix, 0)[0])
+
+        res = term_1 + term_2
+
+        return res
+
+    def cdist(x, y):
+        differences = x.unsqueeze(1) - y.unsqueeze(0)
+        distances = torch.sum(differences**2, -1).sqrt()
+        return distances
+
+
+class DiceBCEHausdorffLoss:
+    def __init__(self, dice_smooth=1.0, bce_weight=0.5):
+        self.bce_weight = bce_weight
+        self.dice_loss = DiceLoss(smooth=dice_smooth)
+        self.hausdorff_loss = AveragedHausdorffLoss()
+
+    def __call__(self, pred, target):
+        bce = F.binary_cross_entropy_with_logits(pred, target)
+        pred = F.sigmoid(pred)
+        dice = self.dice_loss(pred, target)
+        hausdorff = self.hausdorff_loss(pred, target)
+        loss = bce * self.bce_weight + dice * (1 - self.bce_weight) + hausdorff
         return loss
